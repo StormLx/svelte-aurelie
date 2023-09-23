a23f6c385d6fc790deabd2d5783b0b9c9277de0f
Alexandre Caldato <caldato.alexandre@gmail.com>
2023-09-24 00:40:14 +0200

    added some style

--- src/routes/accompagnements/+page.svelte
+++ src/routes/accompagnements/+page.svelte
@@ -1,109 +1,172 @@
-<section id="accompagnements" class="presentation p-5">
+<script>
+    import { BookMarked, Leaf, Cross, AlertTriangle } from 'lucide-svelte';
+</script>
 
-    <div class="section-title">
-        <h2 data-aos="slide-right">Mes accompagnements</h2>
-        <p data-aos="slide-right" data-aos-delay="300">En détail</p>
+<div class="section-title p-5">
+    <h2 data-aos="slide-right">
+        Mes accompagnements
+    </h2>
+    <p>En détail</p>
+</div>
+
+<section id="accompagnements" class="accompagnement p-5">
+
+    <div class="content-section">
+        <div class="content-block">
+            <h3>
+                <BookMarked/>
+                Consultations de soutien psychologique
+            </h3>
+            <p>Mon approche est intégrative, s’appuyant sur différents courants pour tenir compte de la complexité de
+                l’être humain.</p>
+            <p>En évaluant ensemble votre demande, nous établirons s’il s’agit d’un accompagnement psychologique de
+                soutien ponctuel ou d’un accompagnement psychothérapeutique plus long.</p>
+        </div>
+
+        <div class="content-block">
+            <h3>
+                <Leaf/>
+                Séances d'EFT Clinique
+            </h3>
+            <p>L’EFT (Emotionnal Freedom Technique) est une pratique psychocorporelle visant à traiter des blocages
+                émotionnels. Les principes de l’EFT s’appuient sur les fondements des thérapies traditionnelles et
+                ancestrales en
+                alliant psychologie et acupuncture.</p>
+            <details>
+                <summary>Lire plus</summary>
+                <p>
+                    Cette technique qui s’attache au lien entre le corps et l’esprit permet d’apporter un soulagement
+                    émotionnel rapide et durable en traitant la cause de l’émotion négative. Selon le fondateur, Gary
+                    Craig, toutes les émotions négatives sont provoquées par une perturbation de l’équilibre énergétique
+                    du corps. En effet, notre corps serait parcouru par des circuits énergétiques qui peuvent être
+                    fortement perturbés lorsqu’un choc émotionnel survient. Cela va donner lieu à des émotions
+                    négatives qui peuvent être supprimées en rétablissant l’équilibre énergétique du corps.
+                </p>
+                <p>
+                    Ce rééquilibrage se fait en tapotant du bout des doigts les extrémités de certains méridiens
+                    énergétiques issus de l’acupuncture, ce qui va permettre de refaire circuler l’énergie normalement.
+                    Cela contribuerait à réduire la charge émotionnelle associée à un souvenir traumatique, et à changer
+                    l’attitude de l’individu face à son problème.
+                </p>
+            </details>
+        </div>
+
+        <div class="content-block">
+            <h3>
+                <Cross/>
+                Conseils en produits de soins naturels
+            </h3>
+            <p>
+                La phytothérapie, l'aromathérapie ainsi que les produits de la ruche peuvent être un complément aux
+                thérapies psychologiques et énergétiques.
+            </p>
+            <p>
+                Lors d'une séance, si le contexte ou une situation le permet et surtout si cela fait sens pour vous,
+                je peux vous proposer des conseils en produits de soins naturels afin de venir compléter le travail
+                effectué ensemble.
+            </p>
+
+            <details>
+                <summary>Lire plus</summary>
+                <p>
+                    <strong>La phytothérapie</strong>, étymologiquement le traitement par les plantes, est une méthode
+                    thérapeutique
+                    qui utilise l’action des plantes médicinales. Elle fait partie des médecines ancestrale que l'on
+                    utilise
+                    depuis des siècles sous forme de tisanes et plus récemment sous forme d'extrait sec en gélule.
+                </p>
+
+                <p><strong>La gemmothérapie</strong> est une branche de la phytothérapie. Elle s’en distingue néanmoins
+                    par l’usage,
+                    non plus d’un élément de la plante adulte (feuille, écorce, tige, racine, fleur), mais de sa partie
+                    embryonnaire (bourgeon, jeunes pousses, radicelles).
+                </p>
+                <p>Son utilisation permet d’obtenir des thérapeutiques plus actives, il n’est donc pas nécessaire de
+                    prendre des doses importantes.</p>
+                <p><strong>L'aromathérapie</strong> est une approche de soin qui utilise les extraits aromatiques des
+                    plantes, comme
+                    les huiles essentielles, les huiles végétales ou des extraits de plante à des fins thérapeutiques ou
+                    de
+                    bien-être</p>
+                <p>L'aromathérapie peut apporter une aide pour traiter certains troubles mineurs comme le stress, la
+                    fatigue, les problèmes de peau, les migraines ou les douleurs articulaires ou musculaires, la
+                    digestion...
+                </p>
+                <p>Régulatrice à la fois du système neuronal et hormonal, elle agit en profondeur sur le rééquilibrage
+                    de
+                    l'organisme de part ses molécules bio-chimiques.
+                </p>
+                <p>Une huile essentielle bien choisie et bien utilisée pourra aider au lâcher prise et à la détente.</p>
+                <p>Certaines sont apaisantes et relaxantes, pendant que d'autres sont davantage stimulantes et
+                    énergisantes.</p>
+                <p>Leur utilisation est puissante et il est nécessaire de les utiliser avec précaution, c'est pourquoi
+                    j'ai suivi
+                    la formation de conseillère en produits de soins naturels pour vous conseiller au mieux. </p>
+                <p><strong>L'apithérapie</strong> est une thérapeutique douce qui utilise les produits de la ruche,
+                    principalement
+                    le miel,
+                    la propolis, la gelée royale et le pollen à des fins thérapeutiques ou préventives.</p>
+                <p>L'apithérapie est reconnue et utilisée depuis des milliers d'années dans de nombreuses cultures à
+                    travers le monde pour ses différentes vertus et bienfaits.</p>
+                <p>Les produits issus de l'apiculture sont réputés pour leurs propriétés anti-inflammatoires,
+                    antimicrobiennes, cicatrisantes, antioxydantes...</p>
+                <p>Ils contiennent divers composés bioactifs comme des vitamines, des minéraux, des enzymes ou des
+                    acides aminés qui ont un effet positif sur la santé humaine en soutenant et renforçant
+                    l'organisme. </p>
+            </details>
+
+
+        </div>
+
     </div>
 
-
-    <h3>Consultations de soutien psychologique</h3>
-    <p>
-        Mon approche est intégrative. C'est à dire qu'elle ne se réfère pas à un seul modèle théorique mais
-        s’appuie sur différents courants (systémique, humaniste, analytique, neuropsychologique…) afin de
-        tenir compte de la complexité de l’être humain et appréhender au mieux chaque rencontre. <br> <br>
-        En évaluant ensemble votre demande, nous établirons s’il s’agit d’un accompagnement
-        psychologique de soutien ponctuel ou d’un accompagnement psychothérapeutique plus long.
+    <p class="disclaimer">
+        <AlertTriangle/>
+        Toutefois, toutes ces thérapies que l'on peut attribuer aux médecines douces et naturelles ne
+        prétendent pas se substituer à la médecine conventionnelle.
     </p>
-    <h3>Séances d'EFT Clinique</h3>
-    <p>
-        L’EFT (Emotionnal Freedom Technique) dite technique de libération émotionnelle a été créée par
-        Gary Craig aux Etats-Unis dans les années 1990 à partir des découvertes du Dr Roger Callahan.
-    </p>
-    <p>
-        C'est une pratique psychocorporelle visant à traiter des blocages émotionnels et à se libérer des
-        émotions négatives.
-    </p>
-
-    <p>
-        Les principes de l’EFT s’appuient sur les fondements des thérapies traditionnelles et ancestrales en
-        alliant psychologie et acupuncture
-    </p>
-
-    <p>
-        Cette technique qui s’attache au lien entre le corps et l’esprit permet d’apporter un soulagement
-        émotionnel rapide et durable en traitant la cause de l’émotion négative. Selon le fondateur, Gary
-        Craig, toutes les émotions négatives sont provoquées par une perturbation de l’équilibre énergétique
-        du corps. En effet, notre corps serait parcouru par des circuits énergétiques qui peuvent être
-        fortement perturbés lorsqu’un choc émotionnel survient. Cela va donner lieu à des émotions
-        négatives qui peuvent être supprimées en rétablissant l’équilibre énergétique du corps.
-    </p>
-
-    <p>
-        Ce rééquilibrage se fait en tapotant du bout des doigts les extrémités de certains méridiens
-        énergétiques issus de l’acupuncture, ce qui va permettre de refaire circuler l’énergie normalement.
-        Cela contribuerait à réduire la charge émotionnelle associée à un souvenir traumatique, et à changer
-        l’attitude de l’individu face à son problème.
-    </p>
-
-    <h3>Conseils en produits de soins naturels</h3>
-    <p>
-        La phytothérapie, l'aromathérapie ainsi que les produits de la ruche peuvent être un complément aux
-        thérapies psychologiques et énergétiques.
-    </p>
-    <p>
-        Lors d'une séance, si le contexte ou une situation le permet et surtout si cela fait sens pour vous, je
-        peux vous proposer des conseils en produits de soins naturels afin de venir compléter le travail
-        effectué ensemble.
-    </p>
-
-    <p>
-        <strong>La phytothérapie</strong>, étymologiquement le traitement par les plantes, est une méthode thérapeutique
-        qui utilise l’action des plantes médicinales. Elle fait partie des médecines ancestrale que l'on utilise
-        depuis des siècles sous forme de tisanes et plus récemment sous forme d'extrait sec en gélule.
-    </p>
-
-    <p><strong>La gemmothérapie</strong> est une branche de la phytothérapie. Elle s’en distingue néanmoins par l’usage,
-        non plus d’un élément de la plante adulte (feuille, écorce, tige, racine, fleur), mais de sa partie
-        embryonnaire (bourgeon, jeunes pousses, radicelles).
-    </p>
-    <p>Son utilisation permet d’obtenir des thérapeutiques plus actives, il n’est donc pas nécessaire de
-        prendre des doses importantes.</p>
-    <p><strong>L'aromathérapie</strong> est une approche de soin qui utilise les extraits aromatiques des plantes, comme
-        les huiles essentielles, les huiles végétales ou des extraits de plante à des fins thérapeutiques ou de
-        bien-être</p>
-    <p>L'aromathérapie peut apporter une aide pour traiter certains troubles mineurs comme le stress, la
-        fatigue, les problèmes de peau, les migraines ou les douleurs articulaires ou musculaires, la
-        digestion...
-    </p>
-    <p>Régulatrice à la fois du système neuronal et hormonal, elle agit en profondeur sur le rééquilibrage de
-        l'organisme de part ses molécules bio-chimiques.
-    </p>
-    <p>Une huile essentielle bien choisie et bien utilisée pourra aider au lâcher prise et à la détente.</p>
-    <p>Certaines sont apaisantes et relaxantes, pendant que d'autres sont davantage stimulantes et
-        énergisantes.</p>
-    <p>Leur utilisation est puissante et il est nécessaire de les utiliser avec précaution, c'est pourquoi j'ai suivi
-        la formation de conseillère en produits de soins naturels pour vous conseiller au mieux. </p>
-    <p><strong>L'apithérapie</strong> est une thérapeutique douce qui utilise les produits de la ruche, principalement
-        le miel,
-        la propolis, la gelée royale et le pollen à des fins thérapeutiques ou préventives.</p>
-    <p>L'apithérapie est reconnue et utilisée depuis des milliers d'années dans de nombreuses cultures à
-        travers le monde pour ses différentes vertus et bienfaits.</p>
-    <p>Les produits issus de l'apiculture sont réputés pour leurs propriétés anti-inflammatoires,
-        antimicrobiennes, cicatrisantes, antioxydantes...</p>
-    <p>Ils contiennent divers composés bioactifs comme des vitamines, des minéraux, des enzymes ou des
-        acides aminés qui ont un effet positif sur la santé humaine en soutenant et renforçant l'organisme. </p>
-    <p style="color: lightcoral">Toutefois, toutes ces thérapies que l'on peut attribuer aux médecines douces et
-        naturelles ne
-        prétendent pas se substituer à la médecine conventionnelle. En aucun cas, mes conseils n'iront à
-        l'encontre d'un avis médical.</p>
-
-
 </section>
 
-<style>
-    section {
-        background-color: var(--primary-color);
-        color: var(--white-color);
-        text-align: justify;
+<style lang="scss">
+  .accompagnement {
+    text-align: justify;
+    padding: 20px;
+    background-color: var(--primary-color-lighter);
+    color: var(--white-color);
+
+    .section-title {
+      h2 {
+        display: flex;
+        align-items: center;
+        gap: 10px;
+        color: var(--secondary-color);
+      }
     }
+
+    .content-section {
+      .content-block {
+        margin-bottom: 20px;
+
+        h3 {
+          display: flex;
+          align-items: center;
+          gap: 10px;
+          color: var(--secondary-color);
+        }
+
+        details {
+          margin-top: 10px;
+
+          summary {
+            margin-bottom: 10px;
+          }
+        }
+      }
+    }
+
+    .disclaimer {
+      color: lightcoral;
+      margin-top: 20px;
+    }
+  }
 </style>
--- src/routes/presentation/+page.svelte
+++ src/routes/presentation/+page.svelte
@@ -1,137 +1,142 @@
 <script>
     import img from '$lib/assets/lily.jpg';
+    import { Award } from 'lucide-svelte';
 </script>
 
 <section id="presentation" class="presentation p-5">
-    <div class="left-section">
-        <div class="section-title">
-            <h2><i class="bi bi-person-circle"></i> Présentation</h2>
-            <p>En quelques mots</p>
+    <div class="upper-section">
+        <div class="left-section">
+            <div class="section-title">
+                <h2><i class="bi bi-person-circle"></i> Présentation</h2>
+                <p>En quelques mots</p>
+            </div>
+            <p>
+                <strong>Psychologue clinicienne, Praticienne EFT & Conseillère en produits de soins naturels</strong>
+                <br><br>
+                Issue de l’Université Toulouse II, j'ai initialement orienté ma pratique clinique vers la psychologie
+                clinique du vieillissement et la neuropsychologie. Ma curiosité professionnelle m’a amenée à explorer
+                les
+                troubles psychologiques associés au vieillissement, qu'il soit optimal ou pathologique.
+            </p>
+            <p>
+                <i class="bi bi-journal-medical"></i> Mes expériences variées et mes formations complémentaires m'ont
+                ensuite guidée vers le domaine de l'oncologie et des soins palliatifs. Cette immersion m’a confrontée à
+                des
+                questionnements profonds, enrichissant ma pratique et m'incitant à envisager d'autres approches pour mes
+                séances.
+            </p>
         </div>
-
-        <p>
-            <strong>Psychologue clinicienne, Praticienne EFT & Conseillère en produits de soins
-                naturels</strong><br><br>
-            Issue de l’Université Toulouse II, j'ai initialement orienté ma pratique clinique vers la psychologie
-            clinique du vieillissement et la neuropsychologie. Ma curiosité professionnelle m’a amenée à explorer les
-            troubles psychologiques associés au vieillissement, qu'il soit optimal ou pathologique.
-        </p>
-
-        <p>
-            <i class="bi bi-journal-medical"></i> Mes expériences variées et mes formations complémentaires m'ont
-            ensuite guidée vers le domaine de l'oncologie et des soins palliatifs. Cette immersion m’a confrontée à des
-            questionnements profonds, enrichissant ma pratique et m'incitant à envisager d'autres approches pour mes
-            séances.
-        </p>
-
+        <div class="right-section">
+            <img src="{img}" alt="Portrait of Aurélie, the clinician"/>
+        </div>
+    </div>
+    <div class="lower-section">
         <p>
             <i class="bi bi-heart-half"></i> Mon approche clinique se base sur une rencontre présente, centrée sur l’ici
             et maintenant. Je suis là pour vous guider dans votre parcours de vie, en valorisant les ressources
             intérieures que vous possédez déjà. Mon objectif est de renforcer votre autonomie psychique et de vous aider
             à redevenir maître de votre vie.
         </p>
-
         <p>
             <i class="bi bi-patch-check-fill"></i> Outre un soutien psychologique traditionnel, j’offre, lorsque la
             situation s’y prête, des sessions de thérapies brèves de libération émotionnelle (EFT).
         </p>
-
         <p>
             <i class="boxicons bx bxs-leaf"></i> Animée par une passion pour la nature et ses bienfaits, je me suis
             également formée en tant que conseillère en produits de soins naturels. Cette compétence complémentaire me
             permet de vous suggérer des solutions naturelles pour des problématiques courantes telles que l'anxiété, le
             stress ou les troubles du sommeil...
         </p>
-
         <h2 class="my-4"><i class="bi bi-card-list"></i> Diplômes</h2>
         <ul>
-            <li><i class="bi bi-award"></i> 2023: Certification EFT (Emotional Freedom Technique)</li>
-            <li><i class="bi bi-award"></i> 2022: Certification de Conseiller en Produits de Soins Naturels</li>
-            <li><i class="bi bi-award"></i> 2016: DU de Soins Palliatifs</li>
-            <li><i class="bi bi-award"></i> 2013: Master de Psychologie clinique et Gérontologie</li>
-            <li><i class="bi bi-award"></i> 2012: DU de Neuropsychologie</li>
+            <li>
+                <Award/>
+                2023: Certification EFT (Emotional Freedom Technique)
+            </li>
+            <li>
+                <Award/>
+                2022: Certification de Conseiller en Produits de Soins Naturels
+            </li>
+            <li>
+                <Award/>
+                2016: DU de Soins Palliatifs
+            </li>
+            <li>
+                <Award/>
+                2013: Master de Psychologie clinique et Gérontologie
+            </li>
+            <li>
+                <Award/>
+                2012: DU de Neuropsychologie
+            </li>
         </ul>
     </div>
-    <div class="right-section">
-        <img src="{img}" alt="Photo d'Aurélie"/>
-    </div>
-
 </section>
 
 <style lang="scss">
   #presentation {
     text-align: justify;
     display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 
-
+    .upper-section {
+      display: flex;
+      flex-wrap: wrap;
+    }
     .left-section {
-      flex: 0.5;
+      max-width: 730px;
+    }
+
+
+    .left-section, .right-section {
+      flex: 1;
       padding: 20px;
     }
 
     .right-section {
-      flex: 0.5;
-      padding: 20px;
       display: flex;
-      justify-content: center;
-      align-items: flex-start;
+      justify-content: flex-start;
+      align-items: center;
 
       img {
-        overflow: hidden;
         border-radius: 12px;
         object-fit: cover;
         width: 100%;
-        height: 100%;
         max-width: 500px;
-        max-height: 800px;
+        max-height: 500px;
       }
     }
 
-
-    .section-title h2 {
-      font-size: 14px;
-      font-weight: 600;
-      padding: 0;
-      line-height: 1px;
-      margin: 0 0 5px 0;
-      letter-spacing: 2px;
-      text-transform: uppercase;
-    }
-
-    .section-title h2::after {
-      content: "";
-      width: 120px;
-      height: 1px;
-      display: inline-block;
-      margin: 4px 10px;
-    }
-
-    .section-title p {
-      margin: 0;
-      margin: 0;
-      font-size: 36px;
-      font-weight: 700;
-    }
-
-    p {
-      font-weight: 500;
-    }
-
-    h2 {
-      padding: 0;
-      margin: 0;
+    .lower-section {
+      padding: 20px;
+      width: 100%;
+      max-width: 1100px;
     }
 
   }
 
-  @media (max-width: 868px) {
-    .presentation {
-      display: block;
-
-      .right-section {
-        display: none;
-      }
+  @media (min-width: 601px) and (max-width: 1024px) {
+    #presentation .upper-section {
+      flex-direction: row;
     }
   }
 
+  @media (max-width: 800px) {
+    #presentation {
+      .upper-section {
+        flex-direction: column;
+      }
+
+      .left-section, .right-section, .lower-section {
+        padding: 10px;
+      }
+
+      .right-section img {
+        max-width: 100%;
+        max-height: none;
+      }
+    }
+  }
 </style>
