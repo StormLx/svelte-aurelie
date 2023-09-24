521de13af45b4217b4410fb64bfaf8ca9463df23
Alexandre Caldato <caldato.alexandre@gmail.com>
2023-09-24 15:35:06 +0200

    sitemap route

--- src/routes/sitemap.xml/+server.js
+++ src/routes/sitemap.xml/+server.js
@@ -1,6 +1,16 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <url>
+export async function GET() {
+    return new Response(
+        `
+		<?xml version="1.0" encoding="UTF-8" ?>
+		<urlset
+			xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
+			xmlns:xhtml="https://www.w3.org/1999/xhtml"
+			xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0"
+			xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"
+			xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
+			xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
+		>
+			<url>
         <loc>https://aurelie-millet.netlify.app/</loc>
         <lastmod>2023-09-24</lastmod>
         <changefreq>monthly</changefreq>
@@ -30,4 +40,11 @@
         <changefreq>monthly</changefreq>
         <priority>0.8</priority>
     </url>
-</urlset>
+		</urlset>`.trim(),
+        {
+            headers: {
+                'Content-Type': 'application/xml'
+            }
+        }
+    );
+}
